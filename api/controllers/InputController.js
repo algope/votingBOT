9370a5d8d800adbadf0493355279511c55bc1bd2
Alejandro Gonzalez <alejandro.gon.per@gmail.com>
2016-04-15 17:25:33 +0200

    testing

--- api/controllers/InputController.js
+++ api/controllers/InputController.js
@@ -11,41 +11,58 @@ module.exports = {
     var update = req.body;
     var userId = update.message.from.id;
     var command = false;
-    var stage = queries.findOrCreateStage(userId);
     var userName = update.message.from.first_name;
     //var userLast = update.message.from.last_name;
     var userAlias = update.message.from.username;
+    var stage = 0;
 
-    Input.create(update, function (ko, ok) {
-      if (ko) {
-        sails.log.error("[DB] - InputController.js Updates.create error: ", ko);
-      }
-      if (ok) {
-        sails.log.debug("[DB] - InputController.js Updates.create ok: ", ok);
-      }
-    });
+    start().then(getStage).then(processMessage(update).then(filter().then(end())));
 
-    if (update.message.text) {
-      var text = update.message.text;
-      command = commands.processIt(text);
-    } else command = false;
-
-    sails.log.debug("[DEV] - InputController.js Stage: "+stage);
-    switch (stage) {
-      case 1: //start
-        sails.log.debug("[DEV] - Stage 1. Command: "+command);
-        if (update.message.photo || command.commandId == 0 || !command) {
-          answers.answeringError(userId, update, userAlias, user);
-        } else if (command.commandType == 1) {
-          answers.answeringCommandsS1(command, userId, userName);
-        } else {
-          answers.answeringError(userId, update, userAlias, user);
+    function start(){
+      Input.create(update, function (ko, ok) {
+        if (ko) {
+          sails.log.error("[DB] - InputController.js Updates.create error: ", ko);
         }
-        break;
+        if (ok) {
+          sails.log.debug("[DB] - InputController.js Updates.create ok: ", ok);
+        }
+      });
+    }
+
+    function getStage(userId){
+      stage = queries.findOrCreateStage(userId);
+    }
+
+    function processMessage(update){
+      if (update.message.text) {
+        var text = update.message.text;
+        command = commands.processIt(text);
+      } else command = false;
+    }
+
+    function filter(){
+      sails.log.debug("[DEV] - InputController.js Stage: "+stage);
+      switch (stage) {
+        case 1: //start
+          sails.log.debug("[DEV] - Stage 1. Command: "+command);
+          if (update.message.photo || command.commandId == 0 || !command) {
+            answers.answeringError(userId, update, userAlias, user);
+          } else if (command.commandType == 1) {
+            answers.answeringCommandsS1(command, userId, userName);
+          } else {
+            answers.answeringError(userId, update, userAlias, user);
+          }
+          break;
+
+      }
 
     }
 
+
+  function end(){
     return res.ok();
+  }
+
   }
 };
 
