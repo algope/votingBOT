f14d01bf57e4a6a27b5840d4315ed5714979ba4d
Alejandro Gonzalez <alejandro.gon.per@gmail.com>
2016-04-17 14:30:17 +0200

    testing

--- api/services/answers.js
+++ api/services/answers.js
@@ -35,6 +35,7 @@ module.exports.answeringRegisterS0 = function (command, userId, callback_query_i
 module.exports.answeringRegisterS1 = function (command, userId, callback_query_id) {
   telegram.sendMessage(userId, strings.getValidating, "", true, null, {hide_keyboard: true}).then(
     function (response) {
+      sails.log.debug("[DEV] - Answers.js NID: "+command);
       Census.findOne({nid: command}).exec(function (ko, ok){
         if(ok){
           stages.updateStage({user_id: userId}, {stage: 2});
