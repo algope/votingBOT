b1aa083da13bb54b2d59431a983d31cfbf4128e6
Alejandro Gonzalez <alejandro.gon.per@gmail.com>
2016-04-19 13:30:08 +0200

    Vote testing

--- api/controllers/InputController.js
+++ api/controllers/InputController.js
@@ -50,63 +50,63 @@ module.exports = {
     } else command = false;
 
     sails.log.debug("[DEV] - TEXT: " + text);
-    stages.findOrCreateEntry({user_id: userId}, {user_id: userId, stage: 0}).then(process(user));
+    stages.findOrCreateEntry({user_id: userId}, {user_id: userId, stage: 0}).then(
+      function process(user) {
+        if (!user.banned) {
+          sails.log.debug("[DEV] - InputController.js Stage: " + user.stage);
+          if (user.stage == 0) { //start
+            if (!command) {
+              answers.answeringError(userId, update, userAlias, user);
+            } else if (command.commandType == 1) {
+              answers.answeringCommandsS0(command, userId, userName);
+            } else if (command.commandType == 4) {
+              answers.answeringRegisterS0(command, userId, callback_query_id);
+            } else {
+              answers.answeringError(userId, update, userAlias, user);
+            }
 
-    function process(user) {
-      if (!user.banned) {
-        sails.log.debug("[DEV] - InputController.js Stage: " + user.stage);
-        if (user.stage == 0) { //start
-          if (!command) {
-            answers.answeringError(userId, update, userAlias, user);
-          } else if (command.commandType == 1) {
-            answers.answeringCommandsS0(command, userId, userName);
-          } else if (command.commandType == 4) {
-            answers.answeringRegisterS0(command, userId, callback_query_id);
-          } else {
-            answers.answeringError(userId, update, userAlias, user);
+
+          } else if (user.stage == 1) { //Expecting NID
+            if (!command) {
+              answers.answeringError(userId, update, userAlias, user);
+            } else if (command.commandType == 1) {
+              answers.answeringCommandsS1(command, userId, userName);
+            } else if (command.commandType == 5 || command.commandType == 6) {
+              answers.answeringRegisterS1(command, userId, callback_query_id);
+            } else {
+              answers.answeringError(userId, update, userAlias, user);
+            }
+
+          } else if (user.stage == 2) { //Expecting Bdate
+            if (!command) {
+              answers.answeringError(userId, update, userAlias, user);
+            } else if (command.commandType == 1) {
+              answers.answeringCommandsS2(command, userId, userName);
+            } else if (command.commandType == 7) {
+              answers.answeringRegisterS2(command, userId, callback_query_id);
+            } else {
+              answers.answeringError(userId, update, userAlias, user);
+            }
+
+          } else if ((user.stage == 3) && user.valid) { //Ready To vote
+            if (!command) {
+              answers.answeringError(userId, update, userAlias, user);
+            } else if (command.commandType == 1) {
+              answers.answeringCommandsS3(command, userId, userName);
+            } else if (command.commandType == 5 || command.commandType == 6) {
+              answers.answeringVoting(command, userId, callback_query_id);
+            } else {
+              answers.answeringError(userId, update, userAlias, user);
+            }
           }
 
-
-        } else if (user.stage == 1) { //Expecting NID
-          if (!command) {
-            answers.answeringError(userId, update, userAlias, user);
-          } else if (command.commandType == 1) {
-            answers.answeringCommandsS1(command, userId, userName);
-          } else if (command.commandType == 5 || command.commandType == 6) {
-            answers.answeringRegisterS1(command, userId, callback_query_id);
-          } else {
-            answers.answeringError(userId, update, userAlias, user);
-          }
-
-        } else if (user.stage == 2) { //Expecting Bdate
-          if (!command) {
-            answers.answeringError(userId, update, userAlias, user);
-          } else if (command.commandType == 1) {
-            answers.answeringCommandsS2(command, userId, userName);
-          } else if (command.commandType == 7) {
-            answers.answeringRegisterS2(command, userId, callback_query_id);
-          } else {
-            answers.answeringError(userId, update, userAlias, user);
-          }
-
-        } else if ((user.stage == 3) && user.valid) { //Ready To vote
-          if (!command) {
-            answers.answeringError(userId, update, userAlias, user);
-          } else if (command.commandType == 1) {
-            answers.answeringCommandsS3(command, userId, userName);
-          } else if (command.commandType == 5 || command.commandType == 6) {
-            answers.answeringVoting(command, userId, callback_query_id);
-          } else {
-            answers.answeringError(userId, update, userAlias, user);
-          }
+        } else {
+          telegram.sendMessage(userId, strings.getBanned, "", true, null, {hide_keyboard: true});
+          return res.ok();
         }
-
-      } else {
-        telegram.sendMessage(userId, strings.getBanned, "", true, null, {hide_keyboard: true});
-        return res.ok();
       }
+    );
 
-    }
 
     return res.ok();
   }
