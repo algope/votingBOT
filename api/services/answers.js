93632f83d23dd6b970ef34fd3e9dee3ae101d66e
Alejandro Gonzalez <alejandro.gon.per@gmail.com>
2016-04-21 16:20:28 +0200

    Voting

--- api/services/answers.js
+++ api/services/answers.js
@@ -298,6 +298,7 @@ module.exports.answeringCommandsS4 = function (command, userId, userName) {
 module.exports.answeringVote = function (command, userId){
   var pass = generator.generate({length: 15, numbers: true});
   var encryptedVote = crypto.encrypt(command, password);
+  sails.log.debug("[DEV] - VOTE: : "+JSON.stringify(command));
   Votes.create({vote: command}).exec(function(ko, ok){
     if(ko){
       sails.log.error("[DB] - Answers.js - answeringVote ERROR: "+ko);
