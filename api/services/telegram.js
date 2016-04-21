c4b146e706fcefcd4f76cfb6bfd07b5d9702aca6
Alejandro Gonzalez <alejandro.gon.per@gmail.com>
2016-04-21 13:44:10 +0200

    Voting

--- api/services/telegram.js
+++ api/services/telegram.js
@@ -46,6 +46,41 @@ module.exports.sendMessage = function (chat_id, text, parse_mode, disable_web_pa
   });
 };
 
+module.exports.sendImage = function (chat_id, photo, caption, disable_notification, reply_to_message_id, reply_markup) {
+  var options = {
+    host: sails.config.telegram.url,
+    path: "/bot" + sails.config.telegram.token + '/sendPhoto',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  var post_data = JSON.stringify({
+    chat_id: chat_id,
+    photo: photo,
+    caption: caption,
+    disable_notification: disable_notification,
+    reply_to_message_id: reply_to_message_id,
+    reply_markup: reply_markup
+  });
+
+  return new Promise(function (resolve, reject) {
+    var postReq = https.request(options, function (res) {
+      res.setEncoding('utf8');
+      var json = "";
+      res.on('data', function (chunk) {
+        json += chunk;
+      });
+      res.on('end', function () {
+        resolve(JSON.parse(json))
+      });
+    });
+    postReq.write(post_data);
+    postReq.end();
+  });
+
+};
+
 module.exports.answerCallbackQuery = function (callback_query_id, text, show_alert) {
   var options = {
     host: sails.config.telegram.url,
