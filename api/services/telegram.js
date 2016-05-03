f67985c1e1bceb18b6ec3532666744107d9c9621
Alejandro Gonzalez <alejandro.gon.per@gmail.com>
2016-05-03 18:54:24 +0200

    Trying to send photo

--- api/services/telegram.js
+++ api/services/telegram.js
@@ -73,7 +73,7 @@ module.exports.sendPhoto = function (chat_id, photo, caption, disable_notificati
 
   return new Promise(function (resolve, reject) {
     sails.log.debug("INSIDE DA PROMISE!");
-    var url = sails.config.telegram.url+"/bot" + sails.config.telegram.token + '/sendPhoto/';
+    var url = 'https://' + sails.config.telegram.url+"/bot" + sails.config.telegram.token + '/sendPhoto/';
     sails.log.debug("URL : : : : : : : "+url);
     req.post({
       url: url,
