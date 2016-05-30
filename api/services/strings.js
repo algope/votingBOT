cbda5a367f470a7a4412ce0ed13c0a6b4cc0478e
Alejandro Gonzalez <alejandro.gon.per@gmail.com>
2016-05-30 19:25:44 +0200

    Added voting info

--- api/services/strings.js
+++ api/services/strings.js
@@ -10,44 +10,42 @@
 var emoji = require('node-emoji');
 
 module.exports.getVoteOptions = function (locale) {
-  return new Promise(function(resolve, reject){
-    Options.find().exec(function (ko, ok) {
-      if (ok) {
-        var resp = strings.tell('options.intro', locale);
-        for (var i = 0; i < ok.length; i++) {
-          var id = ok[i].id;
-          if(id<10){
-            resp += ok[i].id+"\uFE0F\u20E3" + " : " + ok[i].text + "\n";
-          } else if(id>=10){
-            var idStr = ok[i].id.toString();
-            var n1 = idStr.charAt(0);
-            var n2 = idStr.charAt(1);
-            resp += n1+"\uFE0F\u20E3"+n2+"\uFE0F\u20E3"+ " : " + ok[i].text + "\n";
-          }
+ return new Promise(function(resolve, reject){
+   Options.find().exec(function (ko, ok) {
+     if (ok) {
+       var resp = strings.tell('options.intro', locale);
+       for (var i = 0; i < ok.length; i++) {
+         var id = ok[i].id;
+         if(id<10){
+           resp += ok[i].id+"\uFE0F\u20E3" + " : " + ok[i].text + "\n";
+         } else if(id>=10){
+           var idStr = ok[i].id.toString();
+           var n1 = idStr.charAt(0);
+           var n2 = idStr.charAt(1);
+           resp += n1+"\uFE0F\u20E3"+n2+"\uFE0F\u20E3"+ " : " + ok[i].text + "\n";
+         }
 
-        }
-        resp += strings.tell('options.footer', locale);
-        resolve(resp);
-      }
+       }
+       resp += strings.tell('options.footer', locale);
+       resolve(resp);
+     }
 
-    })
+   })
 
-  })
+ })
 };
 
 module.exports.getVoteText = function (vote){
   return new Promise(function(resolve, reject) {
     var arrayVote = vote.split(",");
     var options = "";
-    Options.find().exec(function (ko, ok) {
-      if (ko) {
-        sails.log.error("[DB] - VotingController.js - optionsFind ERROR: " + ko);
-      } else if (ok) {
-        for (var i = 0; i < arrayVote.length; i++) {
-          options += ok[arrayVote[i]].id + ". " + ok[arrayVote[i]].text;
-        }
-      }
-    });
+    for (var i = 0; i < arrayVote.length; i++) {
+      sails.log.debug("ARRAY IIIIII : : : "+arrayVote[i]);
+      findText(arrayVote[i]).then(function(response){
+        sails.log.debug("RESPONSE"+ response);
+        options+=response.id +". "+response.text;
+      })
+    }
     resolve(options);
   });
 };
@@ -56,3 +54,15 @@ module.exports.tell = function(id, locale, context){
   return emoji.emojify(sails.__({phrase: id, locale: locale}, context));
 };
 
+function findText (id){
+  return new Promise(function(resolve, reject){
+    Options.findOne({id: id}).exec(function (ko, ok) {
+      if (ko) {
+        sails.log.error("[DB] - VotingController.js - optionsFind ERROR: " + ko);
+        reject(ok)
+      } else if (ok) {
+        resolve(ok);
+      }
+    })
+  })
+}
