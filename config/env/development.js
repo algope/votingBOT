7a02ca720c981e1050d06f001a38acd72ddc175c
Alejandro Gonzalez <alejandro.gon.per@gmail.com>
2016-06-03 10:47:07 +0200

    DB fixes

--- config/env/development.js
+++ config/env/development.js
@@ -21,7 +21,7 @@ module.exports = {
     'defaults': 'devMySQL',
     devMySQL: {
       adapter: 'sails-mysql',
-      host: process.env.DB_URL,
+      host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
--- config/env/production.js
+++ config/env/production.js
@@ -16,7 +16,7 @@ module.exports = {
     'defaults': 'prodMySQL',
     prodMySQL:{
       adapter: 'sails-mysql',
-      host: process.env.DB_URL,
+      host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
